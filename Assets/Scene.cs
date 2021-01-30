e2da386addfa054688caaf923fd33736786f0870
TashaSkyUp <TashaSkyUp@gmail.com>
2021-01-30 03:53:19 -0500

    several small things
    -hiding hexgrid until its more functional
    -added place to display title of scene
    -started adding the ability to save/restore scenes

---
+++ Assets/Scene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// this class handles all data needed to run and reproduce a scene:
+// including:
+// Tokens
+// path and file name of map (maybe will save the image data to)
+// Camera position
+// Map name
+// Scale
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
